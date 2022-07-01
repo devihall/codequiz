f92d9551937d94b784e123477b84564d57938613
devihall <devis0306@gmail.com>
2022-07-01 15:10:12 -0400

    start button functionality

---
+++ assets/js/script.js
@@ -0,0 +1,46 @@
+//Main Page variable
+ var mainTextEl = document.querySelector(".mainText");
+ var startQuizBtn = document.querySelector(".start-btn");
+ console.log(startQuizBtn)
+ //var quizContainer = document.createElement("div");
+
+ 
+//  startQuizBtn.addEventListener("click", function(){
+    
+//     debugger
+    
+//     mainTextEl.replaceChildren();
+// })
+
+//console.log("The click listener is working");
+//;
+
+ //function showQuizQuestions() {
+    //var quizContainer = document.createElement("div")
+ //}
+//clicking start button removes other elements
+      //var startQuizBtn= document.querySelector(".start-btn")
+//startQuizBtn.addEventListener ("click", (event) =>{
+    //console.log("The click listener is working");
+    //console.log(event.target);
+    //mainTextEl.replaceChildren();
+
+    // now dynamically create HTML content that shows your quiz question and answer choices
+    //var q1 = document.createElement("h1");
+    // call your function showQuizQuestions
+    //showQuizQuestions();
+//startQuizBtn.remove,
+//mainTextEl.remove
+//})
+
+//function myStartTimerFunction() {
+
+//}
+
+//startQuizBtn.addEventListener("click", myStartTimerFunction)
+
+
+
+
+
+
