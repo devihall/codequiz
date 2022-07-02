d4ee5297b6480b96f495c3638fa2059d95dff625
devihall <devis0306@gmail.com>
2022-07-01 22:00:00 -0400

    div container for questionsappear on click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,65 @@
-//Main Page variable
+////////Main Page variables//////////////////////////
  var mainTextEl = document.querySelector(".mainText");
- var startQuizBtn = document.querySelector(".start-btn");
+ var startQuizBtnEl = document.querySelector(".start-btn");
+ var quizContainerEl= document.querySelector ("#startdiv")
+ var firstQEl=document.querySelector("#firstQ")
+//  console.log ("container")
+//  var end =false;
 
- //var quizContainer = document.createElement("div");
 
- 
- startQuizBtn.addEventListener("click", function(){
-    
-    mainTextEl.replaceChildren();
+
+////remove start page elements on clicking of start button///
+ startQuizBtnEl.addEventListener("click", function(){
+    mainTextEl.remove();
 })
 
-//console.log("The click listener is working");
-//;
+// ////////begin timer on on clicking of start button////////
 
- //function showQuizQuestions() {
-    //var quizContainer = document.createElement("div")
- //}
-//clicking start button removes other elements
-      //var startQuizBtn= document.querySelector(".start-btn")
-//startQuizBtn.addEventListener ("click", (event) =>{
-    //console.log("The click listener is working");
-    //console.log(event.target);
-    //mainTextEl.replaceChildren();
+// startQuizBtnEl.addEventListener("click", timerEl(){
+// })
 
-    // now dynamically create HTML content that shows your quiz question and answer choices
+
+// //////begin the quiz on on clicking of start button///////
+startQuizBtnEl.addEventListener("click", function(){
+    var quizContainerEl= document.createElement("div")
+        quizContainerEl.textContent="i am a div"
+        console.log ("div")
+})
+
+    // startQuizBtnEl.addEventListener("click", function(){    
+    //     var firstQEl = document.createElement("h1");
+    //     firstQEl.className="start-page"
+    //     firstQEl.textContent="Commonly Used data types DO NOT Include:"
+    //     quizContainerEl.appendChild (firstQEl);
+    // })
+
+    
+//  })
+
+ //question One
+//  startQuizBtnEl.addEventListener("click", function(){
+//     //console.log ("hi")
+//  var question = document.createElement("h1");
+//  //console.log ("hi")
+//  question.className = "start-page-title";
+//  question.textContent = "Commonly Used data types DO NOT Include:";
+//  quizContainerEl.appendChild(question);
+//  })
+
+/////////////Any thing below this is a scratchpad//////////
+
+// now dynamically create HTML content that shows your quiz question and answer choices
     //var q1 = document.createElement("h1");
     // call your function showQuizQuestions
     //showQuizQuestions();
-//startQuizBtn.remove,
-//mainTextEl.remove
-//})
 
 //function myStartTimerFunction() {
 
 //}
 
-//startQuizBtn.addEventListener("click", myStartTimerFunction)
+//startQuizBtn.addEventListener("click", myStartTimerFunction)   
+
+
 
 
 
